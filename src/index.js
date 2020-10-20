facee90671a028c9a12bd71e98c5435ee4526697
Lali <lauradiaz1586@gmail.com>
2020-10-20 17:34:48 +0400

    SEO + react helmet

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
+import { helmet } from 'react-helmet';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <helmet>
     <BrowserRouter>
      <App />
     </BrowserRouter>
+    </helmet>
   </React.StrictMode>,
   document.getElementById('root')
 );
