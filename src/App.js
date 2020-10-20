9d7d540bfda637a7383225505eaad97249115e39
Lali <lauradiaz1586@gmail.com>
2020-10-20 17:09:27 +0400

    fixes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Switch } from 'react-router-dom';
 import MainLayout from './components/Layouts/MainLayout';
 import ProjectsLayout from './components/Layouts/ProjectsLayout';
 import AboutMe from './components/AboutMe/index';
+import AboutMeLayout from './components/Layouts/AboutMeLayout';
 
 
 function App() {
@@ -25,9 +26,9 @@ function App() {
           </ProjectsLayout>
         )} />
         <Route path='/aboutMe' render={() => (
-          <ProjectsLayout>
+          <AboutMeLayout>
             <AboutMe />
-          </ProjectsLayout>
+          </AboutMeLayout>
         )} />
       </Switch>
   
