e45ca242dfaaf59beb4bc6321f573963c111358c
Lali <lauradiaz1586@gmail.com>
2021-02-03 11:43:20 +0400

    add animation con contact me

--- src/components/Burger/index.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-//Component
-import { Pivot as Hamburger } from 'hamburger-react';
-
-export default function Burger() {
-  const [isOpen, setOpen] = useState(false);
-  return <Hamburger toggled={isOpen} toggle={setOpen} />;
-}
